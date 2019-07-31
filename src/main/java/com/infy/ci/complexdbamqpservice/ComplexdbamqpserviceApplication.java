ce5597b4e2ced1fb0a62541398d4b495dd804323
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-31 16:41:48 +0530

    first commit

---
+++ src/main/java/com/infy/ci/complexdbamqpservice/ComplexdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.complexdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexdbamqpserviceApplication.class, args);
+	}
+
+}
